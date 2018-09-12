bbbfd050a4d431261b463d7381ea1188e439de08
adam <atomicpenguines@gmail.com>
2018-09-12 12:46:56 +0100

    fixed problem in auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,82 +1,74 @@
-import Autho0Lock from 'auth0-lock'
-
-const authDomain = 'tictacturing.eu.auth0.com'
-const clientId = 'qxUzj8bGiQGcwPYsGsMjomvI6UYElS2o'
+import Auth0Lock from 'auth0-lock'
+const authDomain = 'carlpeaslee.auth0.com'
+const clientId = 'kpcjF5KAIOOGe2Sm4n4NnPFjFhg9YwPI'
 
 class AuthService {
-    constructor() {
-        this.lock = new Auth0Lock(clientId, authDomain, {
-            auth: {
-                params: {
-                    // openid and email available to user
-                    scope: 'openid email'
-                },
-            },
-        })
+	constructor() {
+		this.lock = new Auth0Lock(clientId, authDomain, {
+			auth: {
+				params: {
+					scope: 'openid email'
+				},
+			},
+		})
 
-        this.showLock = this.showLock.bind(this)
+		this.showLock = this.showLock.bind(this)
 
-        this.lock.on('authenticaed', this.authProcess.bind(this))
-    
-    
-        authProcess = (authResult) => {
-            console.log(authResult)
-        }
-    
-        showLock() {
-            this.lock.show()
-        }
+		this.lock.on('authenticated', this.authProcess.bind(this))
+	}
 
+	authProcess = (authResult) => {
+		console.log(authResult)
+	}
 
-        setToken = () => {
-            let {
-                idToken,
-                exp
-            } = authFields
+	showLock() {
+		this.lock.show()
+	}
 
-            localStorage.setItem('idToken', idToken)
-            localStorage.setItem('exp', exp * 1000)
-        }
+	setToken = (authFields) => {
+		let {
+			idToken,
+			exp
+		} = authFields
+		localStorage.setItem('idToken', idToken)
+		localStorage.setItem('exp', exp * 1000)
+	}
 
-        isCurrent = () => {
-            let expString = localStorage.getItem('exp')
-            if (!expString) {
-                localStorage.removeItem('idToken')
-                return false
-            }
-            let now = new Date()
-            // 10 is the radix parameter
-            let exp = new Date(parseInt(expString, 10))
-            if(exp<now) {
-                this.logout()
-                return false
-            } else {
-                return true
-            }
-        }
+	isCurrent = () => {
+		let expString = localStorage.getItem('exp')
+		if (!expString) {
+			localStorage.removeItem('idToken')
+			return false
+		}
+		let now = new Date()
+		let exp = new Date(parseInt(expString, 10)) //10 is radix parameter
+		if ( exp < now ) {
+			this.logout()
+			return false
+		} else {
+			return true
+		}
+	}
 
+	getToken() {
+		let idToken = localStorage.getItem('idToken')
+		if (this.isCurrent() && idToken) {
+			return idToken
+		} else {
+			localStorage.removeItem('idToken')
+			localStorage.removeItem('exp')
+			return false
+		}
+	}
 
-        getToken() {
-            let idToken = localStorage.getItem('idToken')
-            if(this.isCurrent() && idToken) {
-                return idToken
-            } else {
-                localStorage.removeItem('idtoken')
-                localStorage.removeItem('exp')
-                return false
-            }
-        }
-
-        logout = () => {
-            localStorage.removeItem('idtoken')
-            localStorage.removeItem('exp')
-            location.reload()
-        }
-    }
-
+	logout = () => {
+		localStorage.removeItem('idToken')
+		localStorage.removeItem('exp')
+		location.reload()
+	}
 
 }
 
-const auth = new AuthService() 
+const auth = new AuthService()
 
 export default auth
