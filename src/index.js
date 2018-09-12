ab29ee51d48bf7227d850f32a6f838f66ce5ee9c
adam <atomicpenguines@gmail.com>
2018-09-12 12:28:23 +0100

    added some dependencies and imported them in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import Routes from './routes'
 
+import Relay from 'react-relay'
+import userRelay from 'react-router-relay'
+
+// for changing headers
+import {RelayNetworklayer, urlMiddleware} from 'react-relay-networklayer'
+import {relayApi} from './config/endpoints'
+
+
+const creatHeaders = () => {
+
+}
+
 ReactDOM.render(
   <Router
     history={browserHistory}
