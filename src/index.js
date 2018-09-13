8bc28841a51aa82575c10295032ad54a9efd9d02
adam <atomicpenguines@gmail.com>
2018-09-13 14:03:07 +0100

    troubleshooting, fixed some typos

--- src/index.js
+++ src/index.js
@@ -2,51 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, browserHistory, applyRouterMiddleware} from 'react-router'
 import Routes from './routes'
-
 import Relay from 'react-relay'
-import userRelay from 'react-router-relay'
-
+import useRelay from 'react-router-relay'
 // for changing headers
-import {
-  RelayNetworkLayer,
-  urlMiddleware,
-} from 'react-relay-network-layer';
-
+import {RelayNetworkLayer, urlMiddleware} from 'react-relay-network-layer'
 import {relayApi} from './config/endpoints'
-
-
 import auth from './utils/auth'
 
 
 const createHeaders = () => {
   let idToken = auth.getToken()
   if (idToken) {
-    Authorization: `Bearer ${idToken}`
+    return {
+      'Authorization': `Bearer ${idToken}`
+    }
   } else {
     return {}
   }
 }
 
-Relay.injectNetworkLayer [
+Relay.injectNetworkLayer(
   new RelayNetworkLayer([
-    urlMiddleware(
-      {url: (req) => relayApi,}
-    ),
+    urlMiddleware({
+      url: (req) => relayApi,
+    }),
     next => req => {
       req.headers = {
-        // all the original headers, then any new ones
+        // modifies headers
         ...req.headers,
         ...createHeaders()
       }
       return next(req)
     },
   ],{disableBatchQuery: true})
-]
+)
 
 ReactDOM.render(
   <Router
     environment={Relay.Store}
-    render={applyRouterMiddleware(userRelay)}
+    render={applyRouterMiddleware(useRelay)}
     history={browserHistory}
     routes={Routes}
   />,
